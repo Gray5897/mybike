d36c951f8c0cc00e36e5a3b156a09d9288332003
GRUPO BIKE POLI <65693568+diromero25@users.noreply.github.com>
2020-06-14 11:20:25 -0500

    Update user.php
    CAMBIO

--- user.php
+++ user.php
@@ -208,7 +208,7 @@
           </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-default" onclick="clean_user()" name="cancel" id="cancel_act_pac" data-dismiss="modal">Cancelar</button>
-          <button type="submit" class="btn btn-primary" name="save" id="save_pac_edit">GUARDAR</button>    
+          <button type="submit" class="btn btn-primary" name="save" id="save_pac_edit">GUARDAR CAMBIOS</button>    
         </div>
       </div><!-- /.modal-content -->
     </div><!-- /.modal-dialog -->
